f3b0682a8914b249c8cbd0b38eae3a10e20b4ace
DustyJuly <juliet.90@mail.ru>
2020-11-20 00:58:44 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,6 +6,30 @@ $(function(){
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 1225,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+        }
+      }
+    ]
   });
 
   $("a.btn").click(function() {
@@ -17,4 +41,5 @@ $(function(){
     });
     return false;
  });
+
 });
